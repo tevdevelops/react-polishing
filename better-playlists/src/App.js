47a3f99cc9f6448b075d22c9471414a3870ca4f8
Tevin Rivera <tevinrivera94@gmail.com>
2018-12-20 20:45:58 -0500

    Added some basic styling for a slightly cleaner look. May come back to this to make this look more like the Spotify Interface

--- better-playlists/src/App.js
+++ better-playlists/src/App.js
@@ -6,37 +6,6 @@ import HoursCounter from './components/HoursCounter';
 import Filter from './components/Filter';
 import Playlist from './components/Playlist';
 
-let fakeServerData = {
-  user: {
-    name: 'Tevin',
-    playlists: [
-      {
-        name: 'My Favorites',
-        songs: [
-          {name: 'Lost Boy', duration: 200},
-          {name: 'Hard', duration: 205},
-          {name: 'Tadow', duration: 209}
-        ]
-      },
-      {
-        name: 'Party Jams',
-        songs: [
-          {name: 'Fun Song 1', duration: 210},
-          {name: 'Party Song 1', duration: 219},
-          {name: 'Party Song 2', duration: 201}
-        ]
-      },
-      {
-        name: 'LoFi Hip Hop',
-        songs: [
-          {name: 'Chill Song 1', duration: 218},
-          {name: 'Chill Song 2', duration: 209},
-          {name: 'Chill Song 3', duration: 219}
-        ]
-      }
-    ]
-  }
-}
 
 
 class App extends Component {
@@ -114,23 +83,24 @@ class App extends Component {
         })
         : []
     return(
-      <div className="App">
+      <div className="app">
         {this.state.user ?
           <div>
             <h1>{this.state.user.name}'s Playlists</h1>
               <PlaylistCounter playlists={playlistToRender} />
               <HoursCounter playlists={playlistToRender}/>
               <Filter onTextChange={text => this.setState({filterString: text})}/>
+              <div className="playlist-grid">
               {playlistToRender.map(playlist =>
                 <Playlist playlist={playlist} imageUrl={playlist.imageUrl}/>
               )}
-          </div> : <button
+          </div></div> : <button
+                      className="btn spotify-button"
                       onClick={() => {
                         window.location = window.location.href.includes('localhost')
                         ?  'http://localhost:8888/login'
                         : 'https://better-playlists-backend-tev.herokuapp.com/login'}
                       }
-                      style={{padding: '20px', fontSize: '32px', marginTop: '20px'}}
                     >
                       Sign In with Spotfiy
                     </button>
--- better-playlists/src/components/Filter.js
+++ better-playlists/src/components/Filter.js
@@ -6,8 +6,7 @@ class Filter extends Component {
   render() {
     return(
       <div className='filter'>
-        <img />
-        <input type='text' onChange={event => this.props.onTextChange(event.target.value)}/>
+        <input type='text' placeHolder="Search Track or Playlist" onChange={event => this.props.onTextChange(event.target.value)}/>
       </div>
     );
   }
